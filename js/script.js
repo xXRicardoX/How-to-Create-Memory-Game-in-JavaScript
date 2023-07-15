722002606f3556aaf61df96ba022c5058d9a8324
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-07-15 13:56:32 -0300

    Finalizado o jogo da memoria

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-const emojis = ["😊","😊","❤","❤","😍","😍","😂","😂","😒","😒","😎","😎","😜","😜","😉","😉"]
-var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1 )
-for(var i = 0; i < emojis.length; i++){
+const emojis = ["😊", "😊", "❤", "❤", "😍", "😍", "😂", "😂", "😒", "😒", "😎", "😎", "😜", "😜", "😉", "😉"]
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1)
+for (var i = 0; i < emojis.length; i++) {
     let box = document.createElement('div')
     box.className = 'item'
     box.innerHTML = shuf_emojis[i]
+
+    box.onclick = function () {
+        this.classList.add('boxOpen')
+        setTimeout(function () {
+            if (document.querySelectorAll('.boxOpen').length > 1) {
+                if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                        alert("Congratulations, you won!")
+                    }
+                } else {
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                }
+            }
+
+        }, 500)
+    }
+
+
     document.querySelector('.games').appendChild(box)
 }
